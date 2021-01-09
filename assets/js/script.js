4f7b5eca9f1f68ffbceb52c54fccd4d492f8b748
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-01-09 16:55:49 -0700

    adding content to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question = document.querySelector("#question");
+const choices = array.from(document.querySelector("#.choice.text"));
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+const procressBarFull = document.querySelector("#procressBarFull");
