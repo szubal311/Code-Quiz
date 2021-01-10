05a19a65c6cadd8cccd7779ff9aba1ae29cac990
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-01-09 17:35:29 -0700

    added content to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,21 @@ const choices = array.from(document.querySelector("#.choice.text"));
 const progressText = document.querySelector("#progressText");
 const scoreText = document.querySelector("#score");
 const procressBarFull = document.querySelector("#procressBarFull");
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestion = []
+
+let questions = [
+    {
+        question: "Which of the following is not a JavaScript data type?"
+        choice1: "Number",
+        choice2: "Undifined"
+        choice3: "Boolean"
+        choice4: "Float"
+        answer: "Float"
+    }
+
+]
