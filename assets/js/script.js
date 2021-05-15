858061f789ec8df8743775aa22f31e0362fac1ed
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-05-15 09:00:32 -0600

    fixed to to display questions, fixed the timer, and made app functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,242 @@
-import questions from "./questions";
+var timeEl = document.querySelector(".time");
+var startQuiz = document.getElementById("startBtn");
+var startDiv = document.getElementById("startDiv");
+var quizDiv = document.getElementById("quizDiv");
+var correct = document.getElementById("correct");
+var colsingScore = document.getElementById("closingScore");
+var highScore = document.getElementById("highScores");
+var highScorePage = document.getElementById("highScorePage");
+var highScoreList = document.getElementById("highScoreList");
+var answerBtn = document.querySelectorAll(".answerBtn");
+var submit = document.querySelector(".submitBtn");
+var remove = document.querySelector(".removeBtn");
+var timerInterval;
+var scores = [];
+var secondsLeft = 60;
 
-const quizPage = document.getElementById("quizPage");
-const formSubmit = document.querySelector("welcome-form");
-const quizEl = document.getElementById("quiz");
+//question objects
+var Q1 = {
+  qNumber: "Question 1",
+  question: "Which of the following is not a JavaScript data type?",
+  C1: "number",
+  C2: "undifined",
+  C3: "boolean",
+  C4: "float",
+  answer: "C4",
+};
+var Q2 = {
+  qNumber: "Question 2",
+  question: "In which HTML element do we put the JavaScript?",
+  C1: "head",
+  C2: "style",
+  C3: "script",
+  C4: "meta",
+  answer: "C3",
+};
+var Q3 = {
+  qNumber: "Question 3",
+  question: "Which keyword is not a Javascript statement?",
+  C1: "with",
+  C2: "use strict",
+  C3: "debugger",
+  C4: "if",
+  answer: "C2",
+};
+var Q4 = {
+  qNumber: "Question 4",
+  question: "What are the Pop Up box types used in Javascript?",
+  C1: "prompt",
+  C2: "alert",
+  C3: "confirm",
+  C4: "all of the above",
+  answer: "C4",
+};
+var Q5 = {
+  qNumber: "Question 5",
+  question: "Which of the following will write the message 'Hello Smarty Pants'?",
+  C1: "alertBox('Hello Smarty Pants')",
+  C2: "alert(Hello Smarty Pants)",
+  C3: "msgAlert('Hello Smarty Pants')",
+  C4: "alert('Hello Smarty Pants')",
+  answer: "C4",
+};
 
-let newQuestion = questions;
-let answeredQuestion = [];
-let questionCount = 0;
-let quizScore = 0;
+//global var dependant on above objects must be below to function
+var questionsArr = [Q1, Q2, Q3, Q4, Q5];
+// var secondsLeft = questionsArr.length * 15;
+var questionIndex = 0;
 
-formSubmit.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const name = e.target.querySelector("userName").value.trim();
-    if (!name) {
-        return;
+//function for timer
+function setTime() {
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds remaining";
+
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+      endQuiz();
     }
 
-    localStorage.setItem("initals", initials.toUpperCase());
-    const quizMain = document.getElementById("quizMain");
-    quizMain.style.display ="none";
-    topScore();
+  }, 1000);
+}
 
-    const topScore = () => {
-        let userInitials = localStorage.getItem("initials");
-        if (!userInitials) {
-            return;
-        }
+function sendMessage() {
+  timeEl.textContent = "Time's Up!";
+}
 
-        startTimer("start");
+function hideCorrect() {
+  correct.textContent = " ";
+}
 
-        const  nextBtn = quizPage.querySelector(".btn-next");
-        nextBtn.addEventListener("click", nextQuestion.length);
+//on click start button, start time and quiz
+function askQuestions() {
+  startDiv.classList.add("invisible");
+  startDiv.style.display = "none";
+  quizDiv.classList.remove("invisible");
+  quizDiv.classList.add("visible");
+setTime();
+showQuiz();
+}
+
+//function to display each question in quiz
+function showQuiz() {
+  document.getElementById("Q").innerHTML = questionsArr[questionIndex].qNumber + ": " + questionsArr[questionIndex].question;
+  document.getElementById("C1").innerHTML = questionsArr[questionIndex].C1;
+  document.getElementById("C2").innerHTML = questionsArr[questionIndex].C2;
+  document.getElementById("C3").innerHTML = questionsArr[questionIndex].C3;
+  document.getElementById("C4").innerHTML = questionsArr[questionIndex].C4;
+}
+
+//find which button is selected, post if correct, continue questions or end
+for (var i = 0; i < answerBtn.length; i++) {
+    answerBtn[i].addEventListener("click", options);
+}
+
+function options() {
+  var btnId = this.getAttribute("id");
+  
+  isCorrect(btnId);
+  questionIndex++;
+  
+  if (questionIndex <= questionsArr.length - 1) {
+    showQuiz();
+  }
+  else {
+    endQuiz();
+    
+  }
+}
+
+//function for if selected button is correct or incorrect answer
+function isCorrect(btnId) {
+
+  if (btnId == questionsArr[questionIndex].answer) {
+    correct.textContent = "You are Correct!";
+
+  }
+  else {
+    correct.textContent = "Wrong Answer";
+
+    if (secondsLeft > 15) {
+      secondsLeft = secondsLeft - 15;
+    }
+    else {
+      endQuiz();
     }
 
-    const nextQuestion = () => {
-        const currentEl = document.querySelector("li.current");
-        if (currentEl) {
-            calculateScore(currentEl.innerHTML);
-        } else {
-            return;
-        }
+  }
 
-        questionCount++;
-        if (questionCount > newQuestion.length -1) {
-                        // startTimer("stop");
-            // const timeInfo = document.querySelector(".timer span").textContent;
-            // const
-        }
-        show(questionCount);
-    }
+  // clear correct/incorrect message after one second
+  setTimeout(hideCorrect, 1000);
 
-    calculateScore = (userAnswer) => {
-        if (userAnswer === newQuestion[questionCount].answer) {
-            quizPoint += 10;
-            localStorage.setItem("score",`${quizPoint} out of ${newQuestion.length * 10}`);
-        } else {
-            console.log("Incorrect Answer");
-        }
-    }
+}
 
-        displayQuestion = (index) => {
-            let question = document.querySelector(".question");
-            question.innerHTML = `<h4 class="card-title">Q${questionCount +1}.${newQuestion[index].question}</h4>`;
-            const ul =document.createElement("ul");
-            ul.className = "option-group";
-            newQuestion[index].options.forEach(option => {
-                let li =document.createElement("li");
-                li.className = "option";
-                li.textContent = option;
-                li.addEventListener("click", toggleOption)
-                ul.appendChild(li);
-            })
-            question.appendChild("ul");
-        }
 
-        toggleOption = (e) => {
-            const options = document.querySelectorAll("li");
-            options.forEach(options => {
-                if(options.textContent != e.target.textContent) {
-                    option.classList.remove("active")
-                }
-            })
-            e.target.classList.toggleOption("current");
-        }
-})
+
+//Close quiz div and see final score (time is up or all questions answered)
+function endQuiz() {
+  quizDiv.classList.remove("visible");
+  quizDiv.classList.add("invisible");
+  quizDiv.style.display ="none";
+  endScore.classList.remove("invisible");
+  endScore.classList.add("visible");
+  document.getElementById("finalScore").textContent = secondsLeft;
+  clearInterval(timerInterval);
+}
+
+//push score and name to local storage
+function trackHighScore() {
+  var savedScores = JSON.parse(localStorage.getItem("scores"));
+  if (savedScores !== null) {
+    scores = savedScores;
+  }
+  scores.push(document.querySelector(".initialsEntered").value);
+  localStorage.setItem("scores", JSON.stringify(scores));
+
+}
+
+//Get scores out of local storage & add to ul
+function renderScores() {
+  var savedScores = JSON.parse(localStorage.getItem("scores"));
+  if (savedScores !== null) {
+    scores = savedScores;
+  }
+  for (var i = 0; i < scores.length; i++) {
+    scores.sort();
+    scores.reverse();
+    var score = scores[i];
+    var li = document.createElement("li");
+    li.textContent = score;
+    li.setAttribute("data-index", i);
+
+    highScoreList.appendChild(li);
+  }
+
+}
+
+//View high score list
+function highScores() {
+  //on submit, log high score to local storage
+  trackHighScore();
+
+  //clear area and make high score list visible
+  endScore.classList.remove("visible");
+  endScore.classList.add("invisible");
+  endScore.style.display = "none";
+  highScore.classList.remove("invisible");
+  highScore.classList.add("visible");
+
+  renderScores();
+}
+
+function highScorePage() {
+  timeEl.classList.add("invisible");
+  startDiv.classList.add("invisible");
+  startDiv.style.display = "none";
+  quizDiv.classList.remove("visible");
+  quizDiv.classList.add("invisible");
+  quizDiv.style.display = "none";
+  endScore.classList.remove("visible");
+  endScore.classList.add("invisible");
+  endScore.style.display = "none";
+  highScore.classList.remove("invisible");
+  highScore.classList.add("invisible");
+  renderScores();
+}
+
+//Click submit to log and display high scores or high scores link
+submit.addEventListener("click", highScores);
+highScorePage.addEventListener("click", highScorePage);
+
+//Clear scores button
+function clearScore() {
+  localStorage.clear();
+  highScoreList.innerHTML = "";
+}
+
+remove.addEventListener("click", clearScore);
+
+
+//Start button for quiz
+startQuiz.addEventListener("click", askQuestions);
--- assets/js/trivia-end.js
+++ assets/js/trivia-end.js
@@ -1,4 +1,4 @@
-const name = localStorage.getItem("name");
+const userName = localStorage.getItem("userName");
 const score = localStorage.getItem("score");
 const timer = localStorage.getItem("timer")
 
@@ -6,7 +6,7 @@ const nameEl = document.querySelector(".userName");
 const pointsEl = document.querySelector(".userPoints");
 const timerEl = document.querySelector(".userTimer");
 
-if (name && score) {
+if (userName && score) {
     nameEl.textContent = `Nicely Done, ${name}`;
     pointsEl.innerHTML = `Your Score: <span class="points">${score}</span>`;
     timerEl.innerHTML = `Time: <span class ="time">${timer}</span>`;
